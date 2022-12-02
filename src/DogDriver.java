f3858b71ec2d3629969c45e88236cd0c19ce506f
ijmrodriguez <117775856+ijmrodriguez@users.noreply.github.com>
2022-12-02 12:25:35 -0800

    Update DogDriver.java

--- src/DogDriver.java
+++ src/DogDriver.java
@@ -3,14 +3,14 @@ public class DogDriver
     public static void main(String[] args)
     {
         Dog dog1 = new Dog("Fang", 5, "Husky", 80);
-        Dog dog2 = new Dog("Piper", 3, "Austailian Shepherd", 45);
+        Dog dog2 = new Dog("Piper", 3, "Australian Shepherd", 45);
         Dog dog3 = new Dog("Rocket", 3, "Pomeranian", 10);
 
-        System.out.println(dog1.getBreed());
-        System.out.println(dog1.getAge());
-        System.out.println(dog2.getWeight());
-        System.out.println(dog2.kilograms());
+        System.out.println("Breed: " + dog1.getBreed());
+        System.out.println("Age: " + dog1.getAge());
+        System.out.println("Weight: " + dog2.getWeight());
+        System.out.println("Weight: " + dog2.kilograms());
         System.out.println(dog3);
-        System.out.println(dog3.getDogCount());
+        System.out.println("There are: " + dog3.getDogCount() + " dogs");
     }
 }
